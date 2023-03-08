6a0bb6affc8f91b2fdacf5114c7bf1a5a74dc00f
UshaBhat <ushabhatalagodu@gmail.com>
2023-03-08 12:19:45 +0530

    New file added

---
+++ proj1.py
@@ -0,0 +1,21 @@
+# var=input("Enter no")
+# if var==9:
+   # print("yes")
+#else:
+    #print("No")
+
+"""def switch(arg):
+    case={
+       'a': 1,
+       'b': 2,
+    }
+    return case.get(arg,'nothing')
+
+print(switch('b')) """
+i=0
+while i<4:
+    print(i)
+    i=i+1
+    
+
+
